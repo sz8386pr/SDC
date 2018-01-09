a1ac9487f434db988c5bb5252b488b52b02ffe71
sz8386pr <sz8386pr@go.minneapolis.edu>
2018-01-09 16:07:59 -0600

    omdb

--- omdb.py
+++ omdb.py
@@ -1,8 +1,19 @@
 #pip install --user requests
 import requests
+import os
 
-key = ''#something
+'''
+set omdb_key=something
+export omdb_key = something
+'''
+
+key = os.environ['omdb_key']#something
 
 base_url = 'http://www.omdbapi.com/'
 
 movie = input('Movie title')
+
+params = {'apikey' : key, 't' : movie}
+data = requests.get(base_url, params).json()
+
+print(data)
