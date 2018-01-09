3ac0360936151282cc7480c5272fc393328752f3
sz8386pr <sz8386pr@go.minneapolis.edu>
2018-01-09 15:44:02 -0600

    first 2 files

---
+++ guessTheNumber.py
@@ -0,0 +1,13 @@
+import random
+
+rNumber = random.randrange(1, 10)
+
+while True:
+    uNumber = int(input("Guess the number between 1 to 10\n"))
+    if uNumber == rNumber:
+        print("You guessed it right!")
+        break
+    elif uNumber > rNumber:
+        print("Your number is high")
+    elif uNumber < rNumber:
+        print("Your number is low")
